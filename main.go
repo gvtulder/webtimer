39b6a3f9023dce77f3e3555822e11caef19a23bd
Gijs van Tulder <gvtulder@gmail.com>
2025-02-06 01:16:46 +0100

    Embed frontend code in Go build.

--- main.go
+++ main.go
@@ -1,19 +1,44 @@
 package main
 
 import (
+	"embed"
 	"flag"
 	"fmt"
+	"io/fs"
+	"log"
+	"os"
 	"webtimer/server/model"
 	"webtimer/server/server"
 )
 
-var addr = flag.String("addr", ":8000", "http service address")
+var addr string
+var frontendPath string
+
+//go:embed dist/frontend/index.html
+//go:embed dist/frontend/style.css
+//go:embed dist/frontend/main.js
+var embedFrontend embed.FS
+
+func init() {
+	flag.StringVar(&addr, "addr", ":8000", "http service address")
+	flag.StringVar(&frontendPath, "web", "", "frontend resources path")
+	flag.Parse()
+}
 
 func main() {
+	frontend, err := fs.Sub(embedFrontend, "dist/frontend")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if frontendPath != "" {
+		frontend = os.DirFS(frontendPath)
+	}
+
 	timer := model.NewTimer()
 	watch := model.NewTimerWatch(timer)
 
-	fmt.Println("Running at " + *addr)
+	fmt.Println("Running at " + addr)
 	watch.Start()
-	server.RunServer(*addr, watch, timer)
+	server.RunServer(addr, frontend, watch, timer)
 }
--- server/server/server.go
+++ server/server/server.go
@@ -1,17 +1,19 @@
 package server
 
 import (
+	"io/fs"
 	"log"
 	"net/http"
 	"webtimer/server/model"
 )
 
-func RunServer(addr string, watch *model.TimerWatch, timer *model.Timer) {
+func RunServer(addr string, frontend fs.FS, watch *model.TimerWatch, timer *model.Timer) {
 	log.SetFlags(0)
 	hub := newHub()
 	go hub.run(watch, timer)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
 	})
+	http.Handle("/", http.FileServerFS(frontend))
 	log.Fatal(http.ListenAndServe(addr, nil))
 }
